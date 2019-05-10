84a6773f37b2c7516fe675b664d6003517461705
Brian Plantico <bplantico@gmail.com>
2019-05-10 10:02:02 -0600

    IN PROGRESS User story 2 test for astronaut index page is broken

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "As a visitor " do
-
   describe "When I visit astronauts index page, " do
     # (e.g. "Name: Neil Armstrong, Age: 37, Job: Commander")
     it "I see a list of astronauts with name, age, and job for each" do
       astronaut_1 = Astronaut.create!(name: "Neil Armstrong", age: 37, job: "Commander")
+
       visit astronauts_path
 
       within "#astronaut-#{astronaut_1.id}" do
@@ -15,5 +15,15 @@ RSpec.describe "As a visitor " do
       end
     end
 
+    # (e.g. "Average Age: 34")
+    it "the average age of all astronauts"
+      astronaut_1 = Astronaut.create!(name: "Neil Armstrong", age: 37, job: "Commander")
+      astronaut_2 = Astronaut.create!(name: "Arm Neilstrong", age: 34, job: "Assistant To The Regional Commander")
+      astronaut_3 = Astronaut.create!(name: "Strong Neilarm", age: 31, job: "Regional Commander")
+
+      visit astronauts_path
+
+      expect(page).to have_content("Average Age: 34")
+    end
   end
 end
